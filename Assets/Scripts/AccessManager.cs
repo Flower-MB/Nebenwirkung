763b3216f1155a6a4f483c16cca402f5c096a92a
Flower-MB <michael.blum@kabelplus.at>
2025-01-28 20:33:22 +0100

    Update AccessManager.cs

--- Assets/Scripts/AccessManager.cs
+++ Assets/Scripts/AccessManager.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+//A class which manages wether or not you are allowed to enter a location
 public class AccessManager
 {
     //To give or denie Access to rooms!
@@ -8,7 +8,7 @@ public class AccessManager
     public bool hasRepairedElevator = false;
     public bool hasPickedArchiveKey = false;
     public bool hasUnlockedOffice = false;
-
+    //methods which change the boolean value, in order to make locations accessible
     public void PickUpExitKey()
     {
         hasPickedUpExitKey = true;
